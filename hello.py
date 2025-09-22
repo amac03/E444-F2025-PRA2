48da1f05cda65bc83f8d8e20f1c9cc54cc93e96b
amac03 <amac3@bell.net>
2025-09-22 15:00:35 -0400

    example 3-11

--- hello.py
+++ hello.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.errorhandler(404)
 def page_not_found(e):
